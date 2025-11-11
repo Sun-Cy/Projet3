a85abf8fd87b32ffabe02b09a57b3de32136def1
Sun-Cy <37277777+Sun-Cy@users.noreply.github.com>
2025-11-11 11:04:33 -0500

    Added a path to go to the option and change the background menu
    Cant go back yet

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 const GAME_SCENE: PackedScene = preload("uid://b8q4k3g2kge6t")
-
+const OPTION_SCENE: PackedScene = preload("res://scenes/option.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,7 +17,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_option_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(OPTION_SCENE)
 
 
 func _on_quit_button_down() -> void:
