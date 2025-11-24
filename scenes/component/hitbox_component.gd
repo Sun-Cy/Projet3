54c29fe4c846bd5b52e5651f44527b003820351f
Mathys Rioux <mat.stjr@gmail.com>
2025-11-23 21:22:06 -0500

    y-sort

--- scenes/component/hitbox_component.gd
+++ scenes/component/hitbox_component.gd
@@ -1,8 +1,5 @@
-extends Area2D
+extends Node2D
 class_name HitboxComponent
 
-@export var health_component : HealthComponent
-
 func damage(attack: Attack):
-	if health_component:
-		health_component.damage(attack)
+	pass
