f4dab8b878f6189c40cce4220cb7b2d383bfe344
Sun-Cy <37277777+Sun-Cy@users.noreply.github.com>
2025-11-28 13:35:30 -0500

    Ajouter un objet crate pour tester les component

---
+++ scenes/crate.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func build_network_state() -> Dictionary:
+	return {
+		"name": name,
+		"scene": scene_file_path,
+		"state": {
+			"position": global_position,
+		}
+	}
+
+func apply_network_state(state: Dictionary) -> void:
+	global_position = state.position
